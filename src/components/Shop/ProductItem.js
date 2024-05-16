ae0ee225df3ef544eaaacbd2247d8927b93084d6
ayshasiddiqua513 <22it026@skcet.ac.in>
2024-05-16 21:49:09 +0530

    Doing Network calls and andling response the right way

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector  } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../reducer/cartSlice';
@@ -6,13 +6,36 @@ import { cartActions } from '../reducer/cartSlice';
 const ProductItem = (props) => {
   const dispatch=useDispatch();
   const { id,title, price, description } = props;
+  const cart=useSelector((state)=>state.cart);
 
   const addToCartHandler=()=>{
-    dispatch(cartActions.addItemToCart({
-      id,
-      title,
-      price,
-    }));
+    const newTotalQuantity=cart.totalQuantity+1;
+
+    const updatedItems=cart.items.slice();
+    const existingItem=updatedItems.find((item)=>item.id===id);
+    if(existingItem){
+      const updatedItem={...existingItem};
+      updatedItem.quantity++;
+      updatedItem.price=updatedItem.price+price;
+      const existingItemIndex=updatedItems.findIndex(
+        (item)=>item.id===id
+      );
+      updatedItems[existingItemIndex]=updatedItem;
+    }else{
+      updatedItems.push({
+        id:id,
+        price:price,
+        quantity:1,
+        totalPrice:price,
+        name:title,
+      });
+    }
+    const newCart={
+      totalQuantity:newTotalQuantity,
+      items:updatedItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart))
   }
 
   return (
